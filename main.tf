31d70140e81cfa5ca9e322eb4ef303ad57292eab
SriramSRB <ssrirambabu27@gmail.com>
2026-04-20 23:26:13 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 resource "aws_vpc" "library3_vpc" {
-    cidr_blocks = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/16"
     tags        = { Name: "Library3-VPC" }
 }
 
 resource "aws_subnet" "public_subnet" {
     vpc_id                  = aws_vpc.library3_vpc.id
-    cidr_blocks             = "10.0.0.0/21"
+    cidr_block             = "10.0.0.0/21"
     map_public_ip_on_launch = true
     availability_zone       = "ap-south-1a"
     tags                    = { Name: "Library3-Subnet" }                   
@@ -22,13 +22,13 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public_rt" {
     vpc_id = aws_vpc.library3_vpc.id
     route {
-        cidr_blocks = "0.0.0.0/0"
-        gateway_id  = ws_internet_gateway.igw.id
+        cidr_block = "0.0.0.0/0"
+        gateway_id  = aws_internet_gateway.igw.id
     }
 }
 
 resource "aws_route_table_association" "library3_association" {
-    subnet_id      = ws_subnet.public_subnet.id
+    subnet_id      = aws_subnet.public_subnet.id
     route_table_id = aws_route_table.public_rt.id
 }
 
@@ -51,7 +51,7 @@ resource "aws_security_group" "library3_sg" {
         from_port   = 8080
         to_port     = 8080
         protocol    = "tcp"
-        cidr_blocks = {"0.0.0.0/0"}
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         from_port   = 30080
@@ -69,20 +69,20 @@ resource "aws_security_group" "library3_sg" {
 }
 
 resource "aws_key_pair" "library3_key" {
-    key_name   = "library3"
-    public_key = file ("F:\File\Devops\Library3\library3.pub")
+    key_name   = "library3-key"
+    public_key = file("f:/file/devops/library3/library3-key.pub") 
 }
 
-region "aws_instance" "library3_server" {
+resource "aws_instance" "library3_server" {
     ami                    = "ami-05d2d839d4f73aafb"
-    instance_type          = "m71-flex.large"
+    instance_type          = "m7i-flex.large"
     subnet_id              = aws_subnet.public_subnet.id
-    vpc_security_group_ids = aws_security_group.library3_sg.id
+    vpc_security_group_ids = [aws_security_group.library3_sg.id]
     key_name               = aws_key_pair.library3_key.key_name
 
     root_block_device {
         volume_size = 16
         volume_type = "gp3"
     }
-    tags = { Name = "library3-server"}
+    tags = { Name = "library3-server" }
 }
